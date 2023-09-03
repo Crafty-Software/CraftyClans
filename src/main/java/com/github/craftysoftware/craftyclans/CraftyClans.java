8cc129342ddf456bb933c5ca2834c7bae5d7ce6e
hapily04 <hapily04@gmail.com>
2023-09-02 21:28:59 -0500

    Initial commit

---
+++ src/main/java/com/github/craftysoftware/craftyclans/CraftyClans.java
@@ -0,0 +1,18 @@
+package com.github.craftysoftware.craftyclans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftyClans extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
